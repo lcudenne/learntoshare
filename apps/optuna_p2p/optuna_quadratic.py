9647c35706b48d5f99c41fbccd43e828e27dd0d5
Loïc Cudennec <loic.cudennec@gmail.com>
2022-12-02 16:20:42 +0100

    optuna example generic parameters

--- apps/optuna_p2p/optuna_quadratic.py
+++ apps/optuna_p2p/optuna_quadratic.py
@@ -33,6 +33,17 @@ class OptunaAgent():
     def terminate(self):
         self.overlay.terminate()
 
+    def bestParamsTypes(self, best_params):
+        param_types = dict()
+        for param in best_params:
+            if isinstance(best_params[param], float):
+                param_types[param] = "float"
+            elif isinstance(best_params[param], int):
+                param_types[param] = "int"
+            else:
+                param_types[param] = "categorical"
+        return param_types
+
     def dispatchMessage(self, message):
         content_json = json.loads(message.content)
         self.pending_trials.put(content_json)
@@ -53,22 +64,33 @@ class OptunaAgent():
             while not self.pending_trials.empty():
                 trial_json = self.pending_trials.get()
                 trial = study.ask()
-                trial_value = trial_json["best_value"]
+                trial_value = float(trial_json["best_value"])
                 trial_params = trial_json["best_params"]
-                # for param in trial_params:
-                #    trial.storage.get_trial_params(trial._trial_id)[param] = trial_params[param]
-                trial.suggest_float("x", trial_params["x"], float(trial_params["x"]))
-                trial.suggest_int("y", int(trial_params["y"]), int(trial_params["y"]))
-                study.tell(trial, float(trial_value))
+                trial_types = trial_json["param_types"]
+
+                for param in trial_params:
+                    if trial_types[param] == "float":
+                        trial.suggest_float(param,
+                                            float(trial_params[param]), float(trial_params[param]))
+                    elif trial_types[param] == "int":
+                        trial.suggest_int(param,
+                                          int(trial_params[param]), int(trial_params[param]))
+                    else:
+                        print("Warning: categorical distribution not supported. convert to int")
+
+                study.tell(trial, trial_value)
                 print("Add", trial_json, "to local Optuna optimization")
                 
             # optimize locally
             study.optimize(self.objective, n_trials=self.ntrials_per_round)
             print("Round {} best value: {} (params: {})\n".format(r, study.best_value, study.best_params))
+
+            # TOREMOVE
             sleep(2)
-            content = '{"best_value": ' + str(study.best_value) + ', "best_params": ' + str(study.best_params).replace("'", "\"") + '}'
-            
+
+
             # broadcast best trial to other peers
+            content = '{"best_value": ' + str(study.best_value) + ', "best_params": ' + str(study.best_params).replace("'", "\"") + ', "param_types": ' + str(self.bestParamsTypes(study.best_params)).replace("'", "\"") + '}'
             self.overlay.communicator.broadcast(content)
 
         
